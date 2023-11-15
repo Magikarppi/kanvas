52f6f0153d9cbb0ceb3fd0c69ca0682fd49bbca0
Olli Haataja <olli@haataja.eu>
2023-11-15 14:48:24 +0200

    Copypaste initial database service file code from another project

---
+++ back/database/database-service.ts
@@ -0,0 +1,30 @@
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { PG_HOST, PG_PORT, PG_USERNAME, PG_PASSWORD, PG_DATABASE } = process.env;
+
+const pool = new pg.Pool({
+    host: PG_HOST,
+    port: Number(PG_PORT),
+    user: PG_USERNAME,
+    password: String(PG_PASSWORD),
+    database: PG_DATABASE,
+});
+
+export const executeQuery = async (
+    query: string,
+    parameters?: Array<unknown>
+) => {
+    const client = await pool.connect();
+    try {
+        const result = await client.query(query, parameters);
+        return result;
+    } catch (error: any) {
+        console.error(error.stack);
+        error.name = "dbError";
+        throw error;
+    } finally {
+        client.release();
+    }
+};

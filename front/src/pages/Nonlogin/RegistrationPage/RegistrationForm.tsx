6121bb797df65ee74eda2a9266307591f4f04a04
ervjus <jusa1612@gmail.com>
2023-11-17 13:36:29 +0200

    Välipusku onchange jne. tehty

--- front/src/pages/Nonlogin/RegistrationPage/RegistrationForm.tsx
+++ front/src/pages/Nonlogin/RegistrationPage/RegistrationForm.tsx
@@ -1,9 +1,47 @@
 
-import React from "react";
+import React, {useState, ChangeEvent} from "react";
 import Box from "@mui/material/Box";
-import { Container, TextField, Button,Grid, Link, Typography } from "@mui/material";
+import { 
+    Container,
+    TextField, 
+    Button,
+    Grid, 
+    Link, 
+    Typography, 
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    InputAdornment,
+    IconButton  } from "@mui/material";
+import Icons from "../../../components/Icons/Icons";
+
+interface RegistrationFormState {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
 
 const RegistrationForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState<RegistrationFormState>({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const showValues = () => {
+        console.log("First name :" + formData.firstName + " Last name : " + formData.lastName + " Email : " + formData.email + " Password : " + formData.password  );
+    };
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+ 
     return (
         <Container maxWidth="xs">
             <Box
@@ -26,8 +64,10 @@ const RegistrationForm = () => {
                             <TextField
                                 required
                                 name="firstName"
+                                value={formData.firstName}
                                 fullWidth
                                 id="firstName"
+                                onChange={handleInputChange}
                                 label="First Name"
                             />
                         </Grid>
@@ -36,6 +76,8 @@ const RegistrationForm = () => {
                                 required
                                 fullWidth
                                 id="lastName"
+                                value={formData.lastName}
+                                onChange={handleInputChange}
                                 label="Last Name"
                                 name="lastName"
                             />
@@ -45,30 +87,65 @@ const RegistrationForm = () => {
                                 required
                                 fullWidth
                                 id="email"
+                                value={formData.email}
+                                onChange={handleInputChange}
                                 label="Email Address"
                                 name="email"
                                 autoComplete="email"
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                            />
+                            <FormControl fullWidth>
+                                <InputLabel htmlFor="password">Password *</InputLabel>
+                                <OutlinedInput
+                                    id="password"
+                                    type={showPassword ? "text" : "password"}
+                                    onChange={handleInputChange}
+                                    name="password"
+                                    value={formData.password}
+                                    fullWidth
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                edge="end"
+                                                style={{ cursor: "pointer"}}
+                                            >
+                                                {showPassword ? <Icons.VisibilityOff /> : <Icons.Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    label="Password"
+                                />
+                            </FormControl>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="confirmPassword"
-                                label="Confirm Password"
-                                type="password"
-                                id="confirmPassword"
-                            />
+                            <FormControl fullWidth>
+                                <InputLabel htmlFor="confirmPassword">Confirm Password *</InputLabel>
+                                <OutlinedInput
+                                    id="confirmPassword"
+                                    type={showPassword ? "text" : "password"}
+                                    name="confirmPassword"
+                                    value={formData.confirmPassword}
+                                    required
+                                    onChange={handleInputChange}
+                                    fullWidth
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                edge="end"
+                                                style={{ cursor: "pointer"}}
+                                            >
+                                                {showPassword ? <Icons.VisibilityOff /> : <Icons.Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    label="Confirm Password"
+                                />
+                            </FormControl>
                         </Grid>
                     </Grid>
                     <Button
@@ -76,6 +153,7 @@ const RegistrationForm = () => {
                         variant="contained"
                         color="secondary"
                         sx={{ mt: 3, mb: 2 }}
+                        onClick={showValues}
                     >
                     Sign Up
                     </Button>

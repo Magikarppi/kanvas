0cadc0cd57012050a0306c1fa1ba4396954c0624
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-11-17 14:39:45 +0200

    second commit

--- back/server.ts
+++ back/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import { getTeamByIdDao } from "./database/teamsDAO";
 const server = express();
 
 server.use(cors());
 
 server.use(express.json());
 server.use(cookieParser());
+
+server.get('/teams/:id', (req, res) => {
+  const id = req.params.id;
+  const team = getTeamByIdDao(id);
+  res.send(team).status(200);
+});
 
 export default server;

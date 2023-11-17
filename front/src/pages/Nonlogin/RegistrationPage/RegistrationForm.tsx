187737de66fd828ed0db1e8769e86fa0c80ee3be
ervjus <jusa1612@gmail.com>
2023-11-17 08:35:35 +0200

    Registratio form and page UI done

---
+++ front/src/pages/Nonlogin/RegistrationPage/RegistrationForm.tsx
@@ -0,0 +1,97 @@
+
+import React from "react";
+import Box from "@mui/material/Box";
+import { Container, TextField, Button,Grid, Link, Typography } from "@mui/material";
+
+const RegistrationForm = () => {
+    return (
+        <Container maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography variant="h5">
+              Sign up
+                </Typography>
+                <Box
+                    component="form"
+                    //noValidate
+                    //onSubmit={handleSubmit}
+                    sx={{ mt: 3 }}
+                >
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                name="firstName"
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="password"
+                                id="confirmPassword"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+            Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
+
+export default RegistrationForm;

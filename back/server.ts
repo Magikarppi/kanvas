822997f64939e75d2f37974fc7c35dc6b77a139b
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-11-17 15:17:26 +0200

    revert server.ts

--- back/server.ts
+++ back/server.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/usersRouter";
 import { createTablesAndFillWithDummyData } from "./database/database-service";
-import { getTeamByIdDao } from "./database/teamsDao";
 
 const server = express();
 
@@ -12,13 +11,7 @@ server.use(cors());
 server.use(express.json());
 server.use(cookieParser());
 
-server.get('/teams/:id', (req, res) => {
-  const id = req.params.id;
-  const team = getTeamByIdDao(id);
-  res.send(team).status(200);
-});
 server.use("/users", userRouter);
-
 createTablesAndFillWithDummyData();
 
 export default server;

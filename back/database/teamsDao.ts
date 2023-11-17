6fa3a0b64221823cb9557f619a629a4151c86874
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-11-17 13:46:28 +0200

    fix linter

--- back/database/teamsDao.ts
+++ back/database/teamsDao.ts
@@ -1,11 +1,11 @@
-import { executeQuery } from "./database-service"
-import { ITeam } from "./interfaces"
-import { getTeamById } from "./teamQuery"
+import { executeQuery } from "./database-service";
+import { ITeam } from "./interfaces";
+import { getTeamById } from "./teamQuery";
 
 
 export const getTeamByIdDao = async(id: string) => {
-  const teams = await executeQuery(getTeamById, [id]);
-  if(teams){
-    return teams.rows[0] as ITeam
-  }
+    const teams = await executeQuery(getTeamById, [id]);
+    if(teams){
+        return teams.rows[0] as ITeam
+    }
 }

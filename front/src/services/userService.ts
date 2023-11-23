c90fc1af106a05f86b6800f113f2efa9a1688e34
Sami Huuskonen <samih90@gmail.com>
2023-11-23 11:39:42 +0000

    Add function that creates axios client with default headers baseURL and Authorization and use that client

--- front/src/services/userService.ts
+++ front/src/services/userService.ts
@@ -1,51 +1,63 @@
 import axios from "axios";
-import { LoginBody, NewUserBody, UpdatePassWordBody, UpdateUserBodyWithOutPassword } from "../models/userModels";
+import {
+    LoginBody,
+    NewUserBody,
+    UpdatePassWordBody,
+    UpdateUserBodyWithOutPassword,
+} from "../models/userModels";
 
-const usersBaseUrl = "http://localhost:5000/users/";
+const createClient = (token?: string) =>
+    axios.create({
+        baseURL: "http://localhost:5000/users/",
+        headers: { Authorization: `Bearer ${token}` },
+    });
 
 const userRequests = {
     registerUser: async (registerInformation: NewUserBody) => {
-        const request = axios.post(
-            `${usersBaseUrl}signup`,
-            registerInformation
-        );
-        const response = await request;
+        const client = createClient();
+        const response = await client.post("signup", registerInformation);
         return response;
     },
 
     loginUser: async (loginInfo: LoginBody) => {
-        const request = axios.post(`${usersBaseUrl}login`, loginInfo);
-        const response = await request;
+        const client = createClient();
+        const response = await client.post("login", loginInfo);
         return response.data;
     },
 
-    getSingleUser: async (userId: string) => {
-        const request = axios.get(`${usersBaseUrl}${userId}`);
-        const response = await request;
+    getSingleUser: async (token: string, userId: string) => {
+        const client = createClient(token);
+        const response = await client.get(`${userId}`);
         return response.data;
     },
 
-    updateUser: async (userInformation: UpdateUserBodyWithOutPassword) => {
-        const request = axios.put(
-            `${usersBaseUrl}${userInformation.id}`,
+    updateUser: async (
+        token: string,
+        userInformation: UpdateUserBodyWithOutPassword
+    ) => {
+        const client = createClient(token);
+        const response = await client.put(
+            `${userInformation.id}`,
             userInformation
         );
-        const response = await request;
         return response;
     },
 
-    updatePassword: async (updateInformation: UpdatePassWordBody) => {
-        const request = axios.put(
-            `${usersBaseUrl}${updateInformation.id}/password`,
+    updatePassword: async (
+        token: string,
+        updateInformation: UpdatePassWordBody
+    ) => {
+        const client = createClient(token);
+        const response = await client.put(
+            `${updateInformation.id}/password`,
             updateInformation.password
         );
-        const response = await request;
         return response;
     },
 
-    deleteSingleUser: async (userId: string) => {
-        const request = axios.delete(`${usersBaseUrl}${userId}`);
-        const response = await request;
+    deleteSingleUser: async (token: string, userId: string) => {
+        const client = createClient(token);
+        const response = await client.delete(`${userId}`);
         return response;
     },
 };

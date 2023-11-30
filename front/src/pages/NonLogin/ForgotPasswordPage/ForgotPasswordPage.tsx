25ed8bbfd0d880ca64296daf001935e646dbc510
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-11-30 13:27:27 +0200

    EmailError clear

--- front/src/pages/NonLogin/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ front/src/pages/NonLogin/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -29,6 +29,7 @@ export default function ForgotPasswordPage() {
                     email
                 );
                 setEmail("");
+                setEmailError("");
                 setTouched(false);
             }
             else{

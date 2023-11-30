4fac2827c64008596c5a57bc4babd3444bc47d79
k-j-mc <kristofermcc@gmail.com>
2023-11-30 09:39:11 +0200

    Initial kanban drag and drop functionality

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/NonLogin/HomePage/HomePage";
 import LoginPage from "./pages/NonLogin/LoginPage/LoginPage";
 import RegistrationPage from "./pages/NonLogin/RegistrationPage/RegistrationPage";
+import ProjectPage from "./pages/ProjectPage/ProjectPage";
 import UserProfilePage from "./pages/User/UserProfilePage";
 import UnknownUrl from "./pages/ErrorPage/UnknownUrl";
 import NonLoginNavBar from "./components/NavBar/NonLoginNavBar";
@@ -29,6 +30,8 @@ const App = () => {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/sign-in" element={<LoginPage />} />
                     <Route path="/sign-up" element={<RegistrationPage />} />
+
+                    <Route path="/project" element={<ProjectPage />} />
 
                     <Route path="/profile" element={<UserProfilePage />} />
 

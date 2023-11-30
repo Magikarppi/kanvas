0d8262bfd45a71d54fb1c425ad9d9cb53050bcff
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-11-30 12:29:44 +0200

    -

--- front/src/utils/inputChecks.tsx
+++ front/src/utils/inputChecks.tsx
@@ -1,13 +1,5 @@
 import validator from "validator";
 
 export const validEmail = (value: string) => {
-  return validator.isEmail(value) ? true : false;
-};
-
-export const validPassword = (value: string) => {
-  if(value.length < 8){
-    return true;
-  } else {
-    return false;
-  }
+    return validator.isEmail(value) ? true : false;
 };

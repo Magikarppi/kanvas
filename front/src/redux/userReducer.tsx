c9a8063581023aea3f4d829996f5c3d8065cc953
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-12-05 13:26:38 +0200

    Korjauksia

--- front/src/redux/userReducer.tsx
+++ front/src/redux/userReducer.tsx
@@ -5,3 +5,38 @@ export const userReducer = (state = {}, action: any) => {
     }
     return state;
 };
+import { createSlice } from '@reduxjs/toolkit'
+import { ILoginBody } from '../models/userModels';
+import userService from '../services/userService';
+import { AppDispatch } from '../store';
+
+const initialState ={
+    user: null,
+    loadingUser: null,
+}
+
+const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        setUserInfo(state, action) {
+            action.payload = state.user;
+        },
+        setLoading(state, action) {
+            state.loadingUser = action.payload;
+        },
+    },
+})
+
+export const { setUserInfo, setLoading } = userSlice.actions;
+
+export const signInUser = (userPayload: ILoginBody) => {
+    return async (dispatch: AppDispatch) => {
+        await userService
+            .loginUser(userPayload)
+            .then((response) => dispatch(setUserInfo(response)))
+            .catch((error) => console.log(error))
+    }
+};
+
+export default userSlice.reducer;

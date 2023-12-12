9095d9622695b5410da732dfbc06adc848e8837f
Sami Huuskonen <samih90@gmail.com>
2023-12-12 13:11:34 +0000

    Redux user and token state selector 147 + navigate user from login to all-projects-page 141

--- front/src/redux/store.tsx
+++ front/src/redux/store.tsx
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  userReducer  from "./userReducer";
-
+import userReducer from "./userReducer";
 
 const store = configureStore({
     reducer: {
         user: userReducer,
-    }
+    },
 });
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
 export default store;

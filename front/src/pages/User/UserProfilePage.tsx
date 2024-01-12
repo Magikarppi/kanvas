999b739f234ea32298e7604527e72e2844e306b9
AnteroPaivarinta <antsapaivarinta@gmail.com>
2024-01-12 09:47:20 +0200

    kommentoidut

--- front/src/pages/User/UserProfilePage.tsx
+++ front/src/pages/User/UserProfilePage.tsx
@@ -5,18 +5,15 @@ import UserChangePassword from "../../components/UserProfile/UserChangePassword"
 const UserProfilePage = () => {
 
     return (
-        <Grid container spacing={2} className="pageContainer">
-            <Grid item xs={1} />
-            <Grid item xs={10} md={3.5} lg={2} xl={1.5}>
-            </Grid>
-            <Grid item xs={10} md={6.5} lg={8} xl={8.5}>
+        <Grid container spacing={2} className="pageContainer" display="flex" justifyContent="center">
+            <Grid item xs={11} md={9} lg={9} xl={7}>
                 <UserProfile />
                 <UserChangePassword />
             </Grid>
-            <Grid item xs={1} />
         </Grid>
     );
 };
+
 
 
 export default UserProfilePage;

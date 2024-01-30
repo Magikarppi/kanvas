9e5817af960613619870db48e1cbbfab7f01881a
Olli Haataja <ollihaataja1@hotmail.com>
2024-01-30 11:30:06 +0000

    Improve ProjectToolBar.tsx component 215

--- front/src/components/Project/ProjectToolbar/ProjectToolbar.tsx
+++ front/src/components/Project/ProjectToolbar/ProjectToolbar.tsx
@@ -1,44 +1,142 @@
-import { useState } from "react";
-import { Grid, IconButton, Typography } from "@mui/material";
+import React, { SyntheticEvent, useState } from "react";
+import {
+    Autocomplete,
+    Grid,
+    TextField,
+    ToggleButton,
+    ToggleButtonGroup,
+    Tooltip,
+    Typography,
+} from "@mui/material";
 
-import ProjectSearchBar from "./ProjectSearchBar";
 import Icons from "../../Icons/Icons";
 
+const dummyCardOptions = [
+    { title: "Refactor spaghetti code into linguini code" },
+    { title: "Hunt down the elusive memory leak" },
+    { title: "Tame the wild callback jungle" },
+    { title: "Battle CSS specificity wars" },
+    { title: "Herding asynchronous cats" },
+    { title: "Chase after the floating point precision" },
+    { title: "Debugging with a blindfold on" },
+    { title: "Conquer the endless meeting loop" },
+    { title: "Unravel the mystery of NaN" },
+    { title: "Survive the event-driven apocalypse" },
+];
+
+type DisplayType = "grid" | "list";
+const isDisplayType = (string: string): string is DisplayType => {
+    return ["grid", "list"].includes(string);
+};
+
 const ProjectToolbar = () => {
-    const [activeSearch, setActiveSearch] = useState<boolean>(false);
+    const [display, setDisplay] = useState<DisplayType>("grid");
+    const handleDisplay = (
+        _: React.MouseEvent<HTMLElement>,
+        display: string
+    ) => {
+        const isCorrectType = isDisplayType(display);
+        if (isCorrectType) {
+            setDisplay(display);
+        }
+    };
+
+    const handleSelectCard = (_: SyntheticEvent, value: string | null) => {
+        if (value && value.trim() !== "") {
+            // ToDo: Open card modal when card is selected
+            console.log(value);
+        }
+    };
 
     return (
-        <Grid container spacing={1} style={{ padding: "5px 0 5px 0" }}>
-            <Grid item xs={0.5} />
-            <Grid item xs={3}>
-                <IconButton>
-                    <Icons.Grid />
-                </IconButton>
-                <Typography variant="caption">Grid</Typography>
-                <IconButton>
-                    <Icons.List />
-                </IconButton>
-                <Typography variant="caption">List</Typography>
-            </Grid>
-            <Grid item xs={7.5} style={{ textAlign: "right" }}>
-                {activeSearch ? (
-                    <ProjectSearchBar setActiveSearch={setActiveSearch} />
-                ) : (
-                    <>
-                        <IconButton
-                            onClick={() => setActiveSearch(!activeSearch)}
+        <Grid container py={2} alignItems="center">
+            <Grid item xs={12} sm={6}>
+                <ToggleButtonGroup
+                    value={display}
+                    onChange={handleDisplay}
+                    exclusive
+                    sx={{
+                        display: "flex",
+                        justifyContent: {
+                            xs: "center",
+                            sm: "flex-start",
+                        },
+                        marginLeft: {
+                            xs: 0,
+                            sm: "8.5%",
+                        },
+                    }}
+                >
+                    <ToggleButton value="grid" aria-label="grid-view">
+                        <Tooltip
+                            title={<Typography>Show Grid</Typography>}
+                            arrow
+                            enterDelay={350}
                         >
-                            <Icons.Search />
-                        </IconButton>
-                        <Typography variant="caption">Search</Typography>
-                    </>
-                )}
-                <IconButton>
-                    <Icons.Settings />
-                </IconButton>
-                <Typography variant="caption">Settings</Typography>
+                            <Icons.Grid size="24px" />
+                        </Tooltip>
+                    </ToggleButton>
+
+                    <ToggleButton value="list" aria-label="list-view">
+                        <Tooltip
+                            title={<Typography>Show List</Typography>}
+                            arrow
+                            enterDelay={350}
+                        >
+                            <Icons.List size="24px" />
+                        </Tooltip>
+                    </ToggleButton>
+                </ToggleButtonGroup>
             </Grid>
-            <Grid item xs={0.5} />
+            <Grid
+                item
+                xs={12}
+                sm={6}
+                display="flex"
+                alignItems="center"
+                sx={{
+                    display: "flex",
+                    justifyContent: {
+                        xs: "center",
+                        sm: "flex-end",
+                    },
+                    mt: {
+                        xs: 2,
+                        sm: 0,
+                    },
+                }}
+            >
+                <Autocomplete
+                    clearOnEscape
+                    blurOnSelect
+                    onChange={handleSelectCard}
+                    isOptionEqualToValue={(option, value) => option === value}
+                    sx={{
+                        marginRight: {
+                            sm: 5,
+                            xs: 0,
+                        },
+                    }}
+                    ListboxProps={{
+                        sx: {
+                            fontSize: "14px",
+                        },
+                    }}
+                    options={dummyCardOptions.map((card) => card.title)}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            placeholder="Search for a Card"
+                            sx={{
+                                width: "325px",
+                                "& .MuiSvgIcon-root": {
+                                    fontSize: "23px",
+                                },
+                            }}
+                        />
+                    )}
+                />
+            </Grid>
         </Grid>
     );
 };

4732cbd23caa9278cd6b5ce720a22096eb0cfdb6
Jussi Ervasti <jusa1612@gmail.com>
2024-01-31 07:23:15 +0000

    BoardCard muutettu

--- front/src/components/Kanban/BoardCard.tsx
+++ front/src/components/Kanban/BoardCard.tsx
@@ -1,29 +1,123 @@
-import styled from "styled-components";
 import { ICard } from "../../models/cardModels";
 import { Draggable } from "react-beautiful-dnd";
+import { 
+    CardContent,
+    CardActions,
+    Collapse,
+    Typography, 
+    Divider,
+    Box,
+    Avatar,
+    Chip,
+    Stack,
+} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import * as React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { ExpandLess } from "@mui/icons-material";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import { getDate, stringAvatar } from "../../utils/utilFunctions";
 
-const Container = styled.div`
-    border: 1px solid black;
-    padding: 8px;
-    margin-bottom: 10px;
-`;
 
 interface Props {
     card: ICard;
     index: number;
 }
 
+
+const cardStyle = {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "12px",
+    marginBottom: "10px",
+    border: "2px solid",
+    height:"auto",
+    width: "220px",
+    background: "black",
+    paddinLeft: "0px",
+    cursor: "pointer",
+};
+
+const getItemStyle = (isDragging: boolean, draggableStyle?: object) => ({
+    background: isDragging ? "#5e00ff" : cardStyle.background,
+    ...draggableStyle
+});
+
+const dummyUser = {
+    firstName: "Keijo",
+    lastName: "Pekkonen",
+    picture: "dsadsdsa",
+};
+
+const fullName = `${dummyUser.firstName} ${dummyUser.lastName}`;
+
 export default function BoardCard({ card, index }: Props) {
+    const [expanded, setExpanded] = React.useState(false);
+
+
+    const dueDateString: string = card.dueDate?.toString() ?? "";
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <Draggable draggableId={card.id} index={index}>
-            {(provided) => (
-                <Container
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                >
-                    {card.title}
-                </Container>
+        <Draggable draggableId={card.id} index={index} isDragDisabled={expanded}>
+            {(provided, snapshot) => (
+                <>
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={{...cardStyle, ...getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                        ),
+                        }}
+                    >
+                        <CardContent sx={{textAlign: "center", padding:"7px"}}>
+                            <Typography>{card.title}</Typography>
+                            {card.dueDate ? (
+                                <Typography style={{fontSize: "11px"}}>Due {getDate(dueDateString)}</Typography>
+                            ) : (null)}
+                            <Stack direction="row" spacing={1}  sx={{ marginTop: "9px" }}>
+                                <Chip label="primary" size="small" color="primary" />
+                                <Chip label="success" size="small" color="success" />
+                                <Chip label="success" size="small" color="success" />
+                            </Stack>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <Avatar
+                                {...stringAvatar(fullName)}
+                                alt={`${dummyUser.firstName} ${dummyUser.lastName}`}
+                                src={dummyUser.picture as string}
+                                sx={{ width: 26, height: 26,}}
+                            >
+                                {`${dummyUser.firstName[0]}${dummyUser.lastName[0]}`}
+                            </Avatar>
+                            <IconButton aria-label="add to favorites">
+                                <AttachFileIcon />
+                            </IconButton>
+                            <p>2</p>
+                            <IconButton aria-label="share">
+                                <ChatBubbleOutlineIcon />
+                            </IconButton>
+                            <p>2</p> 
+                            {!expanded ? (<ExpandMoreIcon sx={({marginLeft: "auto"})} onClick={handleExpandClick}/>) : (
+                                <ExpandLess sx={({marginLeft: "auto"})} onClick={handleExpandClick}/> 
+                            )}
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <Divider sx={{ marginBottom: "5px" }}/>
+                            <Box sx={{ padding: "14px" , overflowY: "auto", maxHeight: "230px" }}>
+                                <Typography >
+                                    {card.description}
+                                </Typography>
+                            </Box>
+                        </Collapse>
+                    </div>
+                </>
             )}
         </Draggable>
     );
